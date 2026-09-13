9652b5a73cf2dcf198027216b4ed12d3741af004
Liphapangcm <liphapangcm@gmail.com>
2026-09-13 22:17:50 +0200

    Fix CORS error for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://loan-affordability-calculator.onrender.com"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
